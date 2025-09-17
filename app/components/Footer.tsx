b37dd9430d544067e56531d1142bb3d690215c2b
Rico Alexander <rico.alexander@appliedsystems.com>
2025-09-16 21:28:08 -0700

    Updated Contact text

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
             Terms
           </a> */}
           <a href="mailto:thepetswap@gmail.com" className="text-brand-dark hover:underline">
-            Contact
+            Contact Us
           </a>
         </nav>
       </div>
